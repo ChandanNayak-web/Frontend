e8f3739b227e9583d84aab09c1a201f67a65ad9e
N Balgopal Patro <n.balgopalpatra@gmail.com>
2021-06-24 17:36:19 +0530

    Search bar added and fixed the nav bar to top

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $(".slider-one")
-.not(".slick-initialized")
-.slick({
+	.not(".slick-initialized")
+	.slick({
 
-	prevArrow:".site-slider.prev",
-	nextArrow: ".site-slider.next",
-	slidesToShow: 5,
-	slidesToScroll: 1,
-	autoplaySpeed: 3000
-});
+		prevArrow: ".site-slider.prev",
+		nextArrow: ".site-slider.next",
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplaySpeed: 3000
+	});
+
+
